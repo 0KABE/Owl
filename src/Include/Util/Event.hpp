cf45edecf112711385367137078f71a5e1e44fdb
0KABE <RIN.OKAB3@GMAIL.COM>
2021-04-05 16:51:51 +0800

    doc: add documentation for Event

--- src/Include/Util/Event.hpp
+++ src/Include/Util/Event.hpp
@@ -10,10 +10,25 @@ namespace Owl {
 
         Awaitable<void> AsyncWait();
 
+        /**
+         * @brief Notify for all objects awaiting on this event
+         *
+         * @note Objects will not receive notification if start to wait after call NotifyAll()
+         */
         void NotifyAll();
 
+        /**
+         * @brief Notify for one object awaiting on this event
+         *
+         * @note Objects will not receive notification if start to wait after call NotifyAll()
+         */
         void NotifyOnce();
 
+        /**
+         * @brief Continuously notify for any objects awaiting on this event
+         *
+         * @note Objects of which await on this event will receive notification even if ContinuouslyNotify() has been invoked before
+         */
         void ContinuouslyNotify();
 
     private:
